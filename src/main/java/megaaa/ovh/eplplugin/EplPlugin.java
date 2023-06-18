3f0632cbc7f0e9a0cc1f0e81f009d38c9e8f0ef2
Megaaa <martin.gyselinck02@gmail.com>
2023-06-18 13:31:04 +0200

    New AFK command

--- src/main/java/megaaa/ovh/eplplugin/EplPlugin.java
+++ src/main/java/megaaa/ovh/eplplugin/EplPlugin.java
@@ -3,11 +3,14 @@ package megaaa.ovh.eplplugin;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class EplPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.getCommand("coords").setExecutor(new CoordsCommand());
+        Objects.requireNonNull(this.getCommand("coords")).setExecutor(new CoordsCommand());
+        Objects.requireNonNull(this.getCommand("afk")).setExecutor(new AfkCommand());
         this.getServer().getPluginManager().registerEvents(new EventListener(), this);
         Bukkit.getConsoleSender().sendMessage("Plugin enabled");
     }

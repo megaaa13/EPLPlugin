91315483677047189edeedfe97d76c5ae78e7dac
RyuGood0 <ryugood0@gmail.com>
2023-06-18 16:24:58 +0200

    morts scoreboard

--- src/main/java/megaaa/ovh/eplplugin/EplPlugin.java
+++ src/main/java/megaaa/ovh/eplplugin/EplPlugin.java
@@ -6,11 +6,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Objects;
 
 public final class EplPlugin extends JavaPlugin {
+    public static void main(String[] args) {
+
+    }
 
     @Override
     public void onEnable() {
         Objects.requireNonNull(this.getCommand("coords")).setExecutor(new CoordsCommand());
         Objects.requireNonNull(this.getCommand("afk")).setExecutor(new AfkCommand());
+        Objects.requireNonNull(this.getCommand("morts")).setExecutor(new MortScore());
         this.getServer().getPluginManager().registerEvents(new EventListener(), this);
         Bukkit.getConsoleSender().sendMessage("Plugin enabled");
     }
